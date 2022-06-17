0288bd863e044128898310a9a2aaaf8e4662afdd
Daniel Dalcastagne <dalcastagned@gmail.com>
2022-06-17 17:56:45 -0300

    feat: add portfolio section

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,3 +1,5 @@
+import { Data } from '../../data';
+import { Button } from '../../styles/globalStyles';
 import * as S from './styles';
 
 function Portfolio() {
@@ -5,6 +7,27 @@ function Portfolio() {
     <S.Section id="portfolio">
       <h5>Projetos que Realizei</h5>
       <h2>Portfólio</h2>
+
+      <S.ContainerPortfolio>
+        {Data.map(item => (
+          <article key={item.id}>
+            <div>
+              <img src={item.image} alt={item.title} />
+            </div>
+            <h3>{item.title}</h3>
+            <S.CTA>
+              <Button href={item.github} target="_blank">
+                Código Fonte
+              </Button>
+              {item.demo && (
+                <Button href="https://github.com" primary target="_blank">
+                  Live Demo
+                </Button>
+              )}
+            </S.CTA>
+          </article>
+        ))}
+      </S.ContainerPortfolio>
     </S.Section>
   );
 }
--- src/components/Portfolio/styles.js
+++ src/components/Portfolio/styles.js
@@ -1,5 +1,55 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { Container } from '../../styles/globalStyles';
 
 export const Section = styled.section`
   padding-top: 3rem;
+`;
+
+export const ContainerPortfolio = styled(Container)`
+  ${({ theme }) => css`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 2rem;
+    width: 85%;
+    padding: 0 2rem;
+    justify-content: center;
+
+    @media screen and (max-width: 1024px) {
+      width: 100%;
+    }
+
+    article {
+      background: ${theme.colorBackgroundVariant};
+      padding: 2rem;
+      border-radius: 3.2rem;
+      border: 0.1rem solid transparent;
+      transition: ${theme.transition};
+      width: 40rem;
+
+      @media screen and (max-width: 500px) {
+        width: 100%;
+      }
+
+      &:hover {
+        border-color: ${theme.colorPrimaryVariant};
+        background: transparent;
+      }
+    }
+
+    img {
+      height: 16rem;
+      border-radius: 2.4rem;
+      overflow: hidden;
+    }
+
+    h3 {
+      margin: 1.9rem 0 3.2rem;
+    }
+  `}
+`;
+
+export const CTA = styled.div`
+  display: flex;
+  gap: 1.6rem;
+  margin-bottom: 1.6rem;
 `;
